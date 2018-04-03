69c528d0243c4348c5e8c4f22e3d9a60d5b9ae7e
michael <michael@Eugenias-Air.fios-router.home>
2018-04-03 16:32:12 -0400

    basic routing and templates working

--- project/application.py
+++ project/application.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, request, url_for, g
+
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+
+@application.route('/')
+def homepage():
+    return render_template('home.html')
+
+@application.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+@application.route('/cover_letter')
+def cover_letter():
+    return render_template('cover_letter.html')
+
+@application.route('/apps')
+def apps():
+    return render_template('apps.html')
+
 
 # run the app.
 if __name__ == "__main__":
